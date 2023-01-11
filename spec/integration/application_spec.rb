aac10c6c62e9582dac58e67d2b73d44bda2300d8
David Scott <sirdavidscott@hotmail.com>
2023-01-11 15:20:13 +0000

    music library database GET POST artist album

--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 require "rack/test"
-require_relative '../../app'
+require_relative "../../app"
 
 describe Application do
   # This is so we can use rack-test helper methods.
@@ -10,4 +10,58 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  def reset_albums_table
+    seed_sql = File.read("spec/seeds/albums_seeds.sql")
+    connection = PG.connect({ host: "127.0.0.1", dbname: "music_library_test" })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) { reset_albums_table }
+
+  def reset_artists_table
+    seed_sql = File.read("spec/seeds/artists_seeds.sql")
+    connection = PG.connect({ host: "127.0.0.1", dbname: "music_library_test" })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) { reset_artists_table }
+
+  context "GET /albums" do
+    it "returns a list of album titles" do
+      response = get("/albums")
+      expected_response =
+        "Doolittle, Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring"
+      expect(response.status).to eq 200
+      expect(response.body).to eq expected_response
+    end
+  end
+
+  context "POST /albums" do
+    it "creates a new album" do
+      response =
+        post("/albums", title: "Voyage", release_year: "2022", artist_id: "2")
+      expect(response.status).to eq 200
+      expect(response.body).to eq("")
+      response_2 = get("/albums")
+      expect(response_2.body).to include "Voyage"
+    end
+  end
+
+  context "GET /artists" do
+    it "returns a list of artist names" do
+      response = get("/artists")
+      expected_response = "Pixies, ABBA, Taylor Swift, Nina Simone"
+      expect(response.body).to eq expected_response
+    end
+  end
+
+  context "POST /artists" do
+    it "creates a new artist" do
+      response = post("/artists", name: "Wild Nothing", genre: "Indie")
+      expect(response.status).to eq 200
+      expect(response.body).to eq("")
+      response_2 = get("/artists")
+      expect(response_2.body).to include "Wild Nothing"
+    end
+  end
 end
